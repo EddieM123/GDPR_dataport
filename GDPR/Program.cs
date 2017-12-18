ac8160839cfbe8398cf90d7003d4a5535f088db0
Edi <Edi@DESKTOP-1PRKBU7>
2017-12-18 15:07:56 +0100

    Spojena oba dijela

--- GDPR/Program.cs
+++ GDPR/Program.cs
@@ -11,39 +11,70 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.text.html;
 using iTextSharp.text.html.simpleparser;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace GDPR
 {
     class Program
     {
+        private static object console;
+
         static void Main(string[] args)
         {
             MySqlConnection myConnection = new MySqlConnection();
-            myConnection.ConnectionString = "server=localhost;user id=edi;password=password;database=newdb;allowuservariables=True";
+            myConnection.ConnectionString = "server=localhost;user id=root;password=password;database=gdpr;allowuservariables=True";
             myConnection.Open();
 
 
             if (myConnection is null) myConnection.Close();
-            
+
             using (myConnection)
             {
-                String sql = "SELECT * FROM page";
+                string sql = "select * from page";
                 MySqlCommand cmd = new MySqlCommand(sql, myConnection);
                 MySqlDataReader reader = cmd.ExecuteReader();
 
-                //while (reader.Read())
+                //while (reader.read())
                 //{
-                //    String x = reader.GetString("name_hr");
-                //    if (x.Contains("Ministarstvo"))
+                //    string x = reader.getstring("name_hr");
+                //    if (x.contains("ministarstvo"))
                 //    {
-                //        Console.WriteLine(reader.GetString("registrant"));
-                //        Console.WriteLine(reader.GetString("name_en"));
-                //        Console.WriteLine(reader.GetString("keywords"));
+                //        console.writeline(reader.getstring("registrant"));
+                //        console.writeline(reader.getstring("name_en"));
+                //        console.writeline(reader.getstring("keywords"));
                 //    }
                 //}
 
                 reader.Close();
 
+
+
+                // :::::::::::::::::::: DB Backup :::::::::::::::::::::::::
+
+                //string file = "C:/Users/Edi/Desktop/backup.sql";
+                //using (MySqlCommand cmd2 = new MySqlCommand())
+                //{
+                //    using (MySqlBackup mb = new MySqlBackup(cmd2))
+                //    {
+                //        cmd2.Connection = myConnection;
+                //        mb.ExportToFile(file);
+                //        myConnection.Close();
+                //    }
+                //}
+
+
+
+
+
+
+
+
+
+
+
+                // SqlDataReader dr = sqlCommand1.ExecuteReader(CommandBehavior.KeyInfo);
 
                 Console.Write("Unesite hrvatski naziv stranice (name_hr), ili ključnu riječ: ");
                 String wrd = Console.ReadLine();
@@ -75,20 +106,20 @@ namespace GDPR
                     y = row.ItemArray.Select(field => field.ToString()).ToArray();
                     if (y[2].ToLower().Contains(wrd.ToLower()))
                     {
-                            Console.WriteLine("Pronađena stranica: " + y[2] + "     - " + y[1]);
+                        Console.WriteLine("Pronađena stranica: " + y[2] + "     - " + y[1]);
                         string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
                         sb.AppendLine(string.Join(",", fields));
                     }
                 }
 
-                    
-                File.WriteAllText("gdpr.csv", sb.ToString());
+
+                File.WriteAllText("C:/Users/Edi/Desktop/gdpr/gdpr.csv", sb.ToString());
                 Console.WriteLine("CSV stvoren.");
 
                 // ::::::::::::::::::::::::::::::::: PDF :::::::::::::::::::::::::::::::::::::::::::
 
 
-                System.IO.FileStream fs = new FileStream("gdpr.pdf", FileMode.Create);
+                System.IO.FileStream fs = new FileStream("C:/Users/Edi/Desktop/gdpr/gdpr" + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None);
 
                 Document document = new Document(PageSize.A4, 25, 25, 30, 30);
                 PdfWriter writer = PdfWriter.GetInstance(document, fs);
@@ -96,7 +127,7 @@ namespace GDPR
 
                 MySqlDataAdapter sda1 = new MySqlDataAdapter();
                 sda1.SelectCommand = cmd;
-                
+
                 DataTable data1 = new DataTable();
                 sda1.Fill(data1);
 
@@ -115,7 +146,7 @@ namespace GDPR
                 document.Add(new Paragraph(sb1.ToString()));
                 document.Add(new Paragraph(" "));
 
-               
+
                 foreach (DataRow row in data1.Rows)
                 {
                     y = row.ItemArray.Select(field => field.ToString()).ToArray();
@@ -134,10 +165,187 @@ namespace GDPR
                 writer.Close();
                 fs.Close();
 
-                Console.ReadKey();
-                myConnection.Close();
+                // ::::::::: MySqlDump, Situational use
+                // Process.Start(@"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysqldump.exe", ("-u root -p[password] page > test2.sql"));
+
+
+
+
+                Console.WriteLine("Citanje iz dump file-a");
                 
+                myConnection.Close();
+
+
+
+
+
+                // DRUGIO DIO
+
+
+
+
+                Console.Write("Please enter your query: ");
+                string query = System.Console.ReadLine();
+                Regex regex = new Regex(@"\b" + query + @"\b", RegexOptions.IgnoreCase);
+                string line;
+                List<string> columns = new List<string>();
+                List<string> values = new List<string>();
+
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file = new System.IO.StreamReader("C:/Users/Edi/Desktop/gdpr/Backup.sql");
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] rijeci = line.Split();
+                    if (rijeci[0] == "INSERT")
+                    {
+                        //pokupi nazive stupaca
+                        rijeci = (line.Split(new Char[] { '(', ')' }))[1].Split(',');
+                        for (int i = 0; i < rijeci.Length; i++)
+                        {
+                            rijeci[i] = rijeci[i].Trim(new Char[] { ' ', '`', ',' });
+                            if (!columns.Contains(rijeci[i]))
+                            {
+                                columns.Add(rijeci[i]);
+                            }
+                        }
+                    }
+
+                    if (line.Contains("VALUES"))
+                    {
+                        // pokupi sve retke dodane u tablicu
+                        bool end = false;
+                        while (true)
+                        {
+                            line = file.ReadLine();
+                            if (line[line.Length - 1] == ';')
+                            {
+                                end = true;
+                                line = line.Trim(';');
+                            }
+                            values.Add(line.Trim(new Char[] { '(', ')', ' ', '\t', ',' }));
+                            if (end)
+                            {
+                                break;
+                            }
+
+                        }
+
+                    }
+                }
+                List<string> results = new List<string>();
+
+                foreach (string item in values)
+                {
+                    // postoji li traženi pojam u retku
+                    if (regex.IsMatch(item))
+                    {
+                        results.Add(item);
+                    }
+                }
+                if (!results.Any())
+                {
+                    Console.WriteLine("NO MATCHES");
+                    Console.ReadKey();
+                    System.Environment.Exit(0);
+                }
+                //------------------Ispis na konzolu---------------------
+                /*foreach (string item in results)
+                {
+                    List<string> val = splitval(item);
+
+                    for (int i = 0; i < columns.Count(); i++)
+                    {
+                        Console.WriteLine(columns[i] + ": " + val[i]);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }*/
+                // ---------------------------PDF ------------------------------------
+                Console.WriteLine("Stvaram PDF-a");
+
+                BaseFont arial = BaseFont.CreateFont("c:\\windows\\fonts\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                Font NormalFont = new iTextSharp.text.Font(arial, 12, Font.NORMAL);
+                Font boldFont = new iTextSharp.text.Font(arial, 12, iTextSharp.text.Font.BOLD);
+
+                FileStream fs1 = new FileStream("C:/Users/Edi/Desktop/gdpr/" + query + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+                Document doc = new Document();
+                PdfWriter writer1 = PdfWriter.GetInstance(doc, fs1);
+                doc.Open();
+                foreach (string item in results)
+                {
+                    List<string> val = splitval(item);
+
+                    for (int i = 0; i < columns.Count(); i++)
+                    {
+
+                        doc.Add(new Paragraph
+                    {
+                        new Chunk(columns[i] + ": ", boldFont),
+                        new Chunk(val[i], NormalFont
+                        )
+                    });
+
+                    }
+                    doc.Add(new Paragraph("\n"));
+                    doc.Add(new Paragraph("\n"));
+                }
+
+                doc.Close();
+                Console.WriteLine("PDF stvoren\n");
+                //------------------------CSV----------------------------------------
+                Console.WriteLine("Stvaram CSV");
+                string text = "";
+                text = String.Join(",", columns) + ",";
+                foreach (string item in results)
+                {
+                    List<string> val = splitval(item);
+                    text += String.Join(",", val);
+                }
+
+                File.WriteAllText("C:/Users/Edi/Desktop/gdpr/" + query + ".csv", text);
+                Console.WriteLine("CSV stvoren\n");
+
+                file.Close();
+                // Suspend the screen.  
+                System.Console.ReadLine();
             }
+
+            //Podijeli vrijednosti u retku po stupcima
+             
+        }
+
+        static List<string> splitval(String line)
+        {
+            List<string> sval = new List<string>();
+            string value = "";
+            bool ignore = false;
+            bool flag = false;
+            foreach (char character in line)
+            {
+                if (character == '\\')
+                {
+                    flag = true;
+                }
+                else if (character == '\'' && flag == false)
+                {
+                    ignore = !ignore;
+                }
+                else if (character != ',' || ignore == true)
+                {
+                    value += character;
+                    flag = false;
+                }
+                else
+                {
+
+                    sval.Add(value);
+                    value = "";
+                }
+            }
+            sval.Add(value);
+            return sval;
+
+
         }
     }
  }
